730de1b41358d74b823d5ef170b73bfc865809a6
Pavol Šoltés <soltes.pavol@gmail.com>
2019-03-28 21:51:51 +0100

    deleted react, only spring backend left, added example request

--- src/main/java/vava/soltesvasko/lezeckastena/HomeController.java
+++ src/main/java/vava/soltesvasko/lezeckastena/HomeController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/Insult", method=RequestMethod.GET)
+    @ResponseBody
+    public String index(@RequestParam("name") String name) {
+        return "Fuck you sincerely " + name;
     }
 
 }
